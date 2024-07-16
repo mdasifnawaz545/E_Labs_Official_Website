ca470ac20de139b0ccecf96d2e20bde00224a931
Soumyadeep Kundu <soumyadeep.30505@gmail.com>
2024-07-16 20:41:33 +0530

    Created Project Route

---
+++ ELabsOfficalWebsite/src/components/mainComponents/Project.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div className="container mx-auto px-5">
+      <div className="mt-20 mb-20 rounded-xl border-textColor1">
+        <div className="flex flex-wrap items-center justify-evenly">
+          <div className="w-[30%] items-center justify-center flex-col px-6">
+            <img
+              className="aspect-square w-[75%] object-cover rounded-lg"
+              src="https://placehold.co/600x400"
+              alt="project_author_logo"
+            />
+            <div className="flex flex-wrap justify-center items-center mt-8">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
