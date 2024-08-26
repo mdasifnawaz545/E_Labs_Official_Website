9d5b4bba4278c0d552f32c92c079071d31bcccba
Aayush Siwach <22052177@kiit.ac.in>
2024-08-26 19:02:25 +0530

    ham improv

--- src/components/mainComponents/Signup.jsx
+++ src/components/mainComponents/Signup.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
-import github from "../../../public/Images/github.png";
 import { FaGithub, FaGoogle } from "react-icons/fa";
-
 import { SiGithub, SiGoogle } from "react-icons/si";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { ImProfile } from "react-icons/im";
+
 function Signup() {
-    const [username, setUsername] = useState("");
-    const [year, setYear] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const [loading, setLoading] = useState(false);
+    const [googleLoading, setGoogleLoading] = useState(false); // New state for Google loading
+    const [githubLoading, setGithubLoading] = useState(false); // New state for GitHub loading
     const [error, setError] = useState(null);
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        // Allow only numbers and restrict length to 10
+        if (/^\d{0,10}$/.test(value)) {
+            setPhoneNumber(value);
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -21,7 +32,7 @@ function Signup() {
         // Simulate form submission
         setTimeout(() => {
             setLoading(false);
-            if (password == confirmPassword) {
+            if (password === confirmPassword) {
                 setError(null);
             } else {
                 setError("Passwords do not match");
@@ -30,126 +41,146 @@ function Signup() {
                     setError(null);
                 }, 2000);
             }
-            // Replace this with your actual form submission logic
-            if (username !== "test" || password !== "password") {
-                setError("Invalid input. Please check your details.");
-                // Clear error after 2 seconds
-                setTimeout(() => {
-                    setError(null);
-                }, 2000);
-            } else {
-                console.log("Username:", username);
-                console.log("Year:", year);
-                console.log("Email:", email);
-                console.log("Password:", password);
-            }
+            console.log("Email:", email);
+            console.log("Password:", password);
+        }, 1000);
+    };
+
+    const handleGoogleSignIn = () => {
+        setGoogleLoading(true);
+
+        // Simulate Google Sign-In
+        setTimeout(() => {
+            setGoogleLoading(false);
+            // Redirect or handle Google Sign-In result
+            console.log("Signed in with Google");
+        }, 1000);
+    };
+
+    const handleGitHubSignIn = () => {
+        setGithubLoading(true);
+
+        // Simulate Google Sign-In
+        setTimeout(() => {
+            setGithubLoading(false);
+            // Redirect or handle Google Sign-In result
+            console.log("Signed in with GitHub");
         }, 1000);
     };
 
     return (
-    <div className="flex flex-col items-center justify-start">
-
-    
-        <div className="flex scale-75 flex-wrap items-start justify-evenly min-h-screen pt-4 gap-8 dark:bg-blue-100 dark:bg-lightBG">
-            <div className="flex flex-col md:flex-row bg-gradient-to-b from-bgColor to-[#301F0A] dark:from-white dark:to-orange-100 w-[90%] mb-16  border-textColor1 border-[5px] rounded-3xl overflow-auto dark:bg-white">
-                <div className="flex flex-col sm:w-1/2 justify-start p-8 text-textColor1">
-                    <h1 className="md:text-3xl text-xl font-medium mb-8">
-                        SIGN UP
-                    </h1>
-                    <form
-                        onSubmit={handleSubmit}
-                        className="flex flex-col gap-4 justify-center text-center text-lg md:text-xl"
-                    >
-                        <input
-                            type="text"
-                            id="username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            className="flex justify-center items-center py-5 px-7 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-xl w-full leading-tight focus:outline-none focus:shadow-outline text-center text-textColor1 placeholder-textColor1"
-                            placeholder="Enter your Username"
-                            aria-label="Enter your Username"
-                            required
-                        />
-                        <input
-                            type="number"
-                            id="year"
-                            value={year}
-                            onChange={(e) => setYear(e.target.value)}
-                            className="flex justify-center items-center py-5 px-7 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-xl w-full leading-tight focus:outline-none focus:shadow-outline text-center text-textColor1 placeholder-textColor1"
-                            placeholder="Enter Current Year of Studying"
-                            aria-label="Enter Current Year of Studying"
-                            min={1}
-                            max={4}
-                            required
-                        />
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="flex justify-center items-center py-5 px-7 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-xl w-full leading-tight focus:outline-none focus:shadow-outline text-center text-textColor1 placeholder-textColor1"
-                            placeholder="Enter your E-mail"
-                            aria-label="Enter your E-mail"
-                            required
-                        />
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="flex justify-center items-center py-5 px-7 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-xl w-full leading-tight focus:outline-none focus:shadow-outline text-center text-textColor1 placeholder-textColor1"
-                            placeholder="Enter Password"
-                            aria-label="Enter Password"
-                            required
-                        />
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            className="flex justify-center items-center py-5 px-7 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-xl w-full leading-tight focus:outline-none focus:shadow-outline text-center text-textColor1 placeholder-textColor1"
-                            placeholder="Confirm Password"
-                            aria-label="Confirm Password"
-                            required
-                        />
-                        <button className="bg-textColor1  text-black px-5 py-4 w-full rounded-md border-2-textColor1 text-lg md:text-2xl font-medium hover-effect dark:hover:bg-textColor2 hover:bg-bgColor hover:text-textColor1">
-                            {loading ? "Signing up..." : "SIGN UP"}
-                        </button>
-                        <div className="h-6 md:h-4">
-                            {error && <p className="text-red-500">{error}</p>}
-                        </div>
-                        <div className="flex justify-center items-center px-4">
-                            <hr className="w-1/2 h-px bg-textColor2 dark:bg-gray-600 border-0 rounded" />
-                            <span className="mx-2 text-textColor2 dark:text-gray-600">or</span>
-                            <hr className="w-1/2 h-px bg-textColor2 dark:bg-gray-600 border-0 rounded" />
-                        </div>
-                        <a
-                            className="bg-bgColor dark:bg-white text-textColor1 dark:text-black  py-4 w-full rounded-md border-2-textColor1 text-lg md:text-2xl font-medium flex justify-center items-center hover-effect"
-                            href="#"
+        <div className="flex flex-col items-center justify-start">
+            <div className="flex scale-75 flex-wrap items-start justify-evenly min-h-screen pt-4 gap-8 dark:bg-blue-100 dark:bg-lightBG">
+                <div className="flex flex-col md:flex-row bg-gradient-to-b from-bgColor to-[#301F0A] dark:from-white dark:to-orange-100 w-[90%] mb-16 border-textColor1 border-[5px] rounded-3xl overflow-auto dark:bg-white">
+                    <div className="flex flex-col sm:w-1/2 justify-start p-8 text-textColor1">
+                        <p className="md:text-3xl text-xl font-bold mb-8 flex gap-4 items-center">
+                            SIGN UP
+                            <ImProfile />
+                        </p>
+                        <form
+                            onSubmit={handleSubmit}
+                            className="flex flex-col gap-4 justify-center text-center text-lg md:text-xl"
                         >
-                            <p className="w-full text-center flex justify-center items-center gap-3"><FcGoogle/>Sign In with Google</p>
-                        </a>
+                            <input
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="flex justify-center items-center py-2 px-2 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-md w-full leading-tight focus:outline-none focus:shadow-outline text-textColor1 placeholder-textColor1 focus:scale-105"
+                                placeholder="Enter your E-mail"
+                                aria-label="Enter your E-mail"
+                                required
+                            />
+                            <input
+                                type="tel"
+                                id="phoneNumber"
+                                value={phoneNumber}
+                                onChange={handleChange}
+                                className="flex justify-center items-center py-2 px-2 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-md w-full leading-tight focus:outline-none focus:shadow-outline text-textColor1 placeholder-textColor1 focus:scale-105"
+                                placeholder="Enter Phone Number"
+                                aria-label="Enter Phone Number"
+                                required
+                                maxLength="10"
+                            />
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="flex justify-center items-center py-2 px-2 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-md w-full leading-tight focus:outline-none focus:shadow-outline text-textColor1 placeholder-textColor1 focus:scale-105"
+                                placeholder="Enter Password"
+                                aria-label="Enter Password"
+                                required
+                            />
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                value={confirmPassword}
+                                onChange={(e) =>
+                                    setConfirmPassword(e.target.value)
+                                }
+                                className="flex justify-center items-center py-2 px-2 shadow appearance-none border-textColor1 border-2 bg-bgColor dark:bg-white rounded-md w-full leading-tight focus:outline-none focus:shadow-outline text-textColor1 placeholder-textColor1 focus:scale-105"
+                                placeholder="Confirm Password"
+                                aria-label="Confirm Password"
+                                required
+                            />
+                            <button
+                                className="border-2 mt-2 font-bold backdrop-blur-lg hover:shadow-sm hover:shadow-textColor1 hover:scale-105 border-textColor1 duration-100 rounded-md p-1.5 px-2 text-textColor1 focus:scale-105 focus:outline-none"
+                                id="submit"
+                                type="submit"
+                            >
+                                {loading ? "Signing up..." : "SIGN UP"}
+                            </button>
+                            <div className="h-6 md:h-4">
+                                {error && (
+                                    <p className="text-red-500">{error}</p>
+                                )}
+                            </div>
+                            <div className="flex justify-center items-center px-4">
+                                <hr className="w-1/2 h-px bg-textColor2 dark:bg-gray-600 border-0 rounded" />
+                                <span className="mx-2 text-textColor2 dark:text-gray-600">
+                                    or
+                                </span>
+                                <hr className="w-1/2 h-px bg-textColor2 dark:bg-gray-600 border-0 rounded" />
+                            </div>
+                            <button
+                                className="border-2 mt-2 font-bold backdrop-blur-lg hover:shadow-sm hover:shadow-textColor1 hover:scale-105 border-textColor1 duration-100 rounded-md p-1.5 px-2 text-textColor1 focus:scale-105 focus:outline-none"
+                                onClick={handleGoogleSignIn}
+                                type="button" // Prevents form submission
+                            >
+                                <p className="text-center flex justify-center items-center gap-3">
+                                    <FcGoogle />
+                                    {googleLoading
+                                        ? "Signing in with Google..."
+                                        : "Sign In with Google"}
+                                </p>
+                            </button>
+                            <button
+                                className="border-2 mt-2 font-bold backdrop-blur-lg hover:shadow-sm hover:shadow-textColor1 hover:scale-105 border-textColor1 duration-100 rounded-md p-1.5 px-2 text-textColor1 focus:scale-105 focus:outline-none"
+                                onClick={handleGitHubSignIn}
+                                type="button" // Prevents form submission
+                            >
+                                <p className="text-center flex justify-center items-center gap-3">
+                                    <SiGithub />
+                                    {githubLoading
+                                        ? "Signing in with GitHub..."
+                                        : "Sign In with GitHub"}
+                                </p>
+                            </button>
+                        </form>
+                    </div>
+                    <div className="flex flex-col gap-4 justify-center items-center text-center sm:w-1/2 p-10 bg-textColor1">
+                        <h1 className="md:text-4xl text-2xl font-extrabold mb-8">
+                            ALREADY HAVE AN ACCOUNT?
+                        </h1>
                         <a
-                            className="bg-bgColor dark:bg-white text-textColor1 dark:text-black  py-4 w-full rounded-md border-2-textColor1 text-lg md:text-2xl font-medium flex justify-center items-center hover-effect"
-                            href="#"
+                            href="/login"
+                            className="flex justify-center bg-black text-textColor1 px-5 py-4 w-3/4 rounded-md border-2-textColor1 text-lg md:text-2xl font-extrabold hover:scale-105 hover:shadow-sm focus:scale-105 focus:outline-none"
                         >
-
-                        <p className="w-full text-center flex justify-center items-center gap-3"><SiGithub/>Sign In with Google</p>
+                            LOG IN
                         </a>
-                    </form>
+                    </div>
                 </div>
-                <div className="flex flex-col gap-4 justify-center items-center text-center sm:w-1/2 p-10 bg-textColor1">
-                    <h1 className="md:text-4xl text-2xl font-extrabold mb-8">
-                        ALREADY HAVE AN ACCOUNT?
-                    </h1>
-                    <a
-                        href="/login"
-                        className="flex justify-center bg-black text-textColor1 px-5 py-4 w-3/4 rounded-md border-2-textColor1 text-lg md:text-2xl font-medium hover-effect hover:bg-textColor1 hover:text-bgColor hover:outline-black hover:shadow-bgColor"
-                    >
-                        LOG IN
-                    </a>
-                </div>
-            </div>
             </div>
         </div>
     );
