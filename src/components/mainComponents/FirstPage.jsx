366a278815389839b6ccfc1b544b3362fc378bab
Md Asif Nawaz <mdasifnawaz545@gmail.com>
2024-10-30 09:49:48 +0530

    Framer Motion Introduced

--- src/components/mainComponents/FirstPage.jsx
+++ src/components/mainComponents/FirstPage.jsx
@@ -5,7 +5,7 @@ import Member from './Member'
 import Signup from '../mainComponents/Signup'
 import Login from '../mainComponents/Login'
 import EventPage from '../mainComponents/EventPage'
-
+import { motion } from 'framer-motion'
 
 function FirstPage() {
   const widgetRef = useRef();
@@ -17,10 +17,10 @@ function FirstPage() {
           <img src="https://res.cloudinary.com/dpqdgcipi/image/upload/v1719200986/Trasnparent12_1_d7siyr.png" width={256} alt="" />
         </figure>
         <div className="heading flex items-center justify-center flex-col gap-2">
-          <h1 className='text-textColor1 text-center font-black text-5xl '>Empowering through Collaboration:</h1>
-          <h2 className='text-textColor1 text-center font-black text-3xl' >
+          <motion.h1   className='text-textColor1 text-center font-black text-5xl '>Empowering through Collaboration:</motion.h1>
+          <motion.h2  className='text-textColor1 text-center font-black text-3xl' >
             Peer-to-Peer Learning at <span className='text-5xl text-center font-black break-inside-avoid whitespace-nowrap'>E Labs</span>
-          </h2>
+          </motion.h2>
         </div>
         <p className='text-textColor2 max-w-96 text-center'>
           Join us as we foster a community-driven approach to knowledge.
