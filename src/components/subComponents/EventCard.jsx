ebaa5d72a41ac42cdc64d76d49ebf19c67c7d09a
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-12 02:18:26 +0530

    Add dark mode support to EventCard component

--- src/components/subComponents/EventCard.jsx
+++ src/components/subComponents/EventCard.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 function EventCard({ imgSource, eventId, eventName }) {
   return (
     <div className='w-[24rem] scale-90 border border-textColor1 rounded-md h-[24rem] p-4 flex flex-col gap-8'>
-      <div className='overflow-hidden w-88 border-textColor2 h-72 rounded-md border'>
+      <div className='overflow-hidden w-88 border-textColor2 dark:border-black  h-72 rounded-md border'>
         <figure>
           <img src={imgSource} alt={`Event ${eventId}`} />
         </figure>
