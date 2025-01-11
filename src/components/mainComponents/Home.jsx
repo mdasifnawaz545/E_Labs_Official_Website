55f9d8d7ef6c3563c228a4e307ffd3da4ae6e327
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-12 02:18:41 +0530

    Add dark mode background color to Home component

--- src/components/mainComponents/Home.jsx
+++ src/components/mainComponents/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Navbar from './Navbar'
+// import React from 'react'
+// import Navbar from './Navbar'
 import About from './About'
 import FirstPage from './FirstPage'
 import Events from './Events'
 import Member from './Member'
 import Feedback from './Feedback'
 import Gallery from './Gallery'
-import Members from './Members'
+// import Members from './Members'
 
 function Home() {
 
   return (
-    <div className='body dark:bg-blue-100 dark:backdrop-blur-lg'>
+    <div className='body dark:bg-[#ffd4b3] dark:backdrop-blur-lg'>
       <section id='home' className='min-h-screen flex flex-col justify-center items-center'>
         <FirstPage />
       </section>
