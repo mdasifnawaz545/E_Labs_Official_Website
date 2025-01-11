560afc17625f7ff789f8375eb2ee9d1b5e2dc38d
Soumyadip Maity <22053029@kiit.ac.in>
2025-01-12 02:44:41 +0530

    Add dark mode support to FeedbackCard component

--- src/components/subComponents/FeedbackCard.jsx
+++ src/components/subComponents/FeedbackCard.jsx
@@ -7,10 +7,10 @@ function FeedbackCard({
   imgSource = "https://res.cloudinary.com/dpqdgcipi/image/upload/v1720237114/1676480037898_jypfgp.jpg",
 }) {
   return (
-    <div className="flex flex-col items-center justify-between p-4 gap-4 rounded-br-3xl border-2 border-textColor1">
-      <p className="text-textColor2 text-[18px]">{feedback}</p>
+    <div className="flex flex-col items-center justify-between p-4 gap-4 rounded-br-3xl border-2 border-textColor1 ">
+      <p className="text-textColor2 dark:text-black text-[18px]">{feedback}</p>
       <div className="w-full flex items-center justify-end gap-2">
-        <figure className="rounded-full">
+        <figure className="rounded-full dark:text-black">
           {/* <img className="rounded-full aspect-square object-cover" src={imgSource} alt="" width={24} height={24} /> */}
           <FaRegUser />
 
